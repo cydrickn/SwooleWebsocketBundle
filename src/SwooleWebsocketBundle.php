bb8841b4ee631cb0493dc294634a2cc4ffc06087
Cydrick Nonog <cydrick.dev@gmail.com>
2022-10-01 01:04:41 +0800

    fix: initialize the server

--- src/SwooleWebsocketBundle.php
+++ src/SwooleWebsocketBundle.php
@@ -6,15 +6,29 @@ use Cydrickn\SocketIO\Router\Route;
 use Cydrickn\SwooleWebsocketBundle\Attribute\RouteAttribute;
 use Cydrickn\SwooleWebsocketBundle\DependencyInjection\Compiler\RoutePass;
 use Cydrickn\SwooleWebsocketBundle\DependencyInjection\SwooleWebsocketExtension;
+use Cydrickn\SwooleWebsocketBundle\Runtime\Runtime;
+use Cydrickn\SwooleWebsocketBundle\Websocket\Server;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SwooleWebsocketBundle extends AbstractBundle
 {
+    public function boot()
+    {
+        parent::boot();
+        if ($this->container->has(Server::class)) {
+            $this->container->get(Server::class);
+        } elseif ($this->container->has(\Cydrickn\SocketIO\Server::class)) {
+            $this->container->get(\Cydrickn\SocketIO\Server::class);
+        }
+    }
+
     public function build(ContainerBuilder $container)
     {
         $container->registerAttributeForAutoconfiguration(
